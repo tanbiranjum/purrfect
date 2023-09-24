558e604cd6568bd0fbd8e5e0d5881efb36c1315b
tanbiranjum <tanbiranjum@hotmail.com>
2023-09-24 06:42:20 +0600

    map dynamically imported

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,16 +1,13 @@
-"use client"
-
 import React from "react"
 import L, { LatLngTuple } from "leaflet"
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 
 import "leaflet/dist/leaflet.css"
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
 import markerIcon from "leaflet/dist/images/marker-icon.png"
 import markerShadow from "leaflet/dist/images/marker-shadow.png"
 
-import ClientOnly from "../client-only"
 import ChangeView from "./change-view"
+import dynamic from "next/dynamic"
 
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl
@@ -28,13 +25,16 @@ type Props = {
 }
 
 const Map = ({ location, className, zoom, children }: Props) => {
+  const MapContainer = dynamic(async ()=> (await import('react-leaflet')).MapContainer, {ssr: false})
+  const Marker = dynamic(async ()=> (await import('react-leaflet')).Marker, {ssr: false})
+  const Popup = dynamic(async ()=> (await import('react-leaflet')).Popup, {ssr: false})
+  const TileLayer = dynamic(async ()=> (await import('react-leaflet')).TileLayer, {ssr: false})
   return (
-    <ClientOnly>
       <MapContainer
         center={(location as LatLngTuple) || [51.505, -0.09]}
         zoom={zoom || 13}
         scrollWheelZoom={false}
-        className={`h-[35vh] rounded-lg ${className}`}
+        className={`h-80 rounded-lg ${className}`}
       >
         <ChangeView center={location as LatLngTuple} zoom={13} />
         <TileLayer
@@ -46,7 +46,6 @@ const Map = ({ location, className, zoom, children }: Props) => {
         </Marker>
         {children}
       </MapContainer>
-    </ClientOnly>
   )
 }
 
